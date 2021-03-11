a65dc804bcbc2cbae5d74606a0b3635ac96fa56b
bkl14 <78179650+bkl14@users.noreply.github.com>
2021-03-11 10:42:47 -0600

    Update qlibdump.py

--- src/qlibdump.py
+++ src/qlibdump.py
@@ -1,4 +1,4 @@
 import pandas as pd
-data = pd.read_csv('MSFT_2004-10-31_2020-06-12_minute.csv')
+data = pd.read_csv('sampleMSFTdata.csv')
 data = data.drop_duplicates(subset='t', keep='first')
 data.to_csv("MSFT.csv", index=False)
